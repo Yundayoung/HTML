dd8e52ef5c7bfcf0a81be931e44926aa83c9c2c0
admin <rtdy22@gmail.com>
2025-04-30 09:29:46 +0900

    7주차차

--- js/js_login.js
+++ js/js_login.js
@@ -20,6 +20,31 @@ const check_input = () => {
         return false;
     }
 
+    if (emailValue.length < 5) {
+        alert('아이디는최소5글자이상입력해야합니다.');
+        return false;
+    }
+        
+    if (passwordValue.length < 12) {
+        alert('비밀번호는 반드시 12글자이상 입력해야합니다.');
+        return false;
+    }
+        
+    const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\=\[\]{};':"\\|,.<>\/?]+/) !== null;
+        
+    if (!hasSpecialChar) {
+    alert('패스워드는 특수문자를 1개이상 포함해야합니다.');
+    return false;
+    }
+        
+    const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
+    const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
+    
+        if (!hasUpperCase || !hasLowerCase) {
+    alert('패스워드는 대소문자를 1개이상 포함해야합니다.');
+    return false;
+    }
+       
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
     loginForm.submit();
