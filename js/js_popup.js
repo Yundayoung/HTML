6c34c6eb87be16859c42cba894bdc45747d0afb4
admin <rtdy22@gmail.com>
2025-06-07 17:01:22 +0900

    ..

--- js/js_popup.js
+++ js/js_popup.js
@@ -53,7 +53,7 @@ function closePopup() {
 function pop_up() {
     var cookieCheck = getCookie("popupYN");
     if (cookieCheck != "N"){
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    window.open("./popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 }
 
